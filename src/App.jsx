e7e89a2092d9fa8088efc1c0d5a3c06bb0474666
oerd bej <oerdbej@gmail.com>
2024-05-30 23:52:17 +0200

    adding the header and some minor changes to Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Navbar from "./component/Navbar/Navbar";
+import Header from "./component/Header/Header";
 
 const App = () => {
   return (
     <div>
       <Navbar />
+      <Header />
     </div>
   );
 };
--- src/component/Navbar/Navbar.jsx
+++ src/component/Navbar/Navbar.jsx
@@ -3,7 +3,9 @@ import React from "react";
 const Navbar = () => {
   return (
     <div className='h-30 flex items-center w-full text-white'>
-      <div>ob</div>
+      <div className='text-3xl pl-20' font-bold>
+        ob
+      </div>
     </div>
   );
 };
