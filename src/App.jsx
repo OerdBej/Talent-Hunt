f9da8ced6a9d9243a21f37084ece913feab74e99
oerd bej <oerdbej@gmail.com>
2024-05-30 23:58:53 +0200

    working on search bar and removing readme text

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "./component/Navbar/Navbar";
 import Header from "./component/Header/Header";
+import SearchBar from "./component/SearchBar/SearchBar";
 
 const App = () => {
   return (
     <div>
       <Navbar />
       <Header />
+      <SearchBar />
     </div>
   );
 };
---
+++ src/component/SearchBar/SearchBar.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const SearchBar = () => {
+  return (
+    <div>
+      <select>
+        <option value='' disabled>
+          first option
+        </option>
+        <option value=''>first option</option>
+        <option value=''>first option</option>
+      </select>
+    </div>
+  );
+};
+
+export default SearchBar;
