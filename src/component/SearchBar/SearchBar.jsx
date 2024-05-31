024a14a124a372f33ecdb1508869a6f35da4a6d0
oerd bej <oerdbej@gmail.com>
2024-05-31 23:57:00 +0200

    adding the option values for the dropwdown

--- src/component/SearchBar/SearchBar.jsx
+++ src/component/SearchBar/SearchBar.jsx
@@ -3,12 +3,15 @@ import React from "react";
 const SearchBar = () => {
   return (
     <div>
-      <select>
-        <option value='' disabled>
-          first option
+      <select className='w-54 pl-4 py-3 bg-zinc-200 font-semibold rounded-md'>
+        <option value='' disabled hidden selected>
+          Jobs
         </option>
-        <option value=''>first option</option>
-        <option value=''>first option</option>
+        <option value='Frontend'>Frontend</option>
+        <option value='Backend'>Backend</option>
+        <option value='iOS Developer'>iOS Developer</option>
+        <option value='Android Developer'>Android Developer</option>
+        <option value='Advocate'>Advocate</option>
       </select>
     </div>
   );
