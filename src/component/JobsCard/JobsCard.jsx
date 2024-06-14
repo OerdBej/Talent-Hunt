5b10589024d6159ca3ac5c0dbce5f96716822dfb
oerd bej <oerdbej@gmail.com>
2024-06-14 23:46:55 +0200

    adding some flex to the jobscards div

--- src/component/JobsCard/JobsCard.jsx
+++ src/component/JobsCard/JobsCard.jsx
@@ -8,12 +8,12 @@ const JobsCard = () => {
   const differentDays = date.diff("2024-06-6", "day");
 
   return (
-    <div>
-      <div>
+    <div className="mx-40 mb-4">
+      <div className="flex justify-between items-center px-6 bg-zinc-200 rounded-md border border-black shadow-lg hover:border-blue-500 hover:translate-y-1 hover:scale-100">
         <div>
           <h1>Frontend developer - Apple</h1>
           <p>Full Time - Junior - In-Office</p>
-          //tags skills map
+          {/* tags skills map */}
           <div>
             {skills.map((skills) => {
               <p>{skills}</p>;
@@ -21,10 +21,10 @@ const JobsCard = () => {
           </div>
           `
         </div>
-        //here the days that posted
+        {/* here the days that posted */}
         <div>
-                  <p>Posted on {differentDays} </p>
-                  <button>Apply</button>
+          <p>Posted on {differentDays} </p>
+          <button>Apply</button>
         </div>
       </div>
     </div>
