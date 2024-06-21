19b0b23a7795e2fbe2b8fa1db6f5ffbf94c5037e
oerd bej <oerdbej@gmail.com>
2024-06-21 21:36:45 +0200

    setting up firebase and also the fetchjobs async funciton to get the data from database

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 
 TalentHunt is an online job search portal built with React, styled using Tailwind CSS, and powered by Firebase for backend services.
 dfasd;fh;ashdfh;asd;hfa;hsdf;hasd;hfasdf
+
+//collection data doclumentation : https://cloud.google.com/firestore/docs/create-database-web-mobile-client-library
--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Navbar from './component/Navbar/Navbar';
 import Header from './component/Header/Header';
 import SearchBar from './component/SearchBar/SearchBar';
@@ -6,7 +6,34 @@ import JobsCard from './component/JobsCard/JobsCard';
 // the fake api data dummy
 import jobsData from './component/Jobs_API/jobs_api';
 
+//get multiple documents from a collection
+import { collection, query, where, getDocs } from 'firebase/firestore';
+//data in our database
+import { db } from './Firebase/firebase.config';
+
 const App = () => {
+  const [jobsData, setJobsData] = useState([]);
+
+  //async to fetch jobs - firebase
+
+  const fetchJobs = async () => {
+    try {
+      // Querying data from the collection
+      const q = query(collection(db, 'jobs'));
+      const querySnapshot = await getDocs(q);
+      //to get all the data
+      querySnapshot.forEach((doc) => {
+        // doc.data() is never undefined for query doc snapshots
+        console.log(doc.id, ' => ', doc.data());
+      });
+    } catch (error) {
+      console.error('Error fetching jobs: ', error);
+    }
+  };
+
+  //display in ui to fetch the page is loaded with new job
+  useEffect(() => {}, []);
+
   return (
     <div>
       <Navbar />
